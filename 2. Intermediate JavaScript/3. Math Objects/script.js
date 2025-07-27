67b4d0b2864b49a7551edd75b784ecbf7e0cf2d2
Heet Gudhka <heetgudhka24@gmail.com>
2025-07-27 19:24:15 +0530

    JavaScript Interview Series

---
+++ 2. Intermediate JavaScript/3. Math Objects/script.js
@@ -0,0 +1,47 @@
+// Math Objects in JavaScript: The Math namespace object contains static properties and methods for mathematical constants and functions. It only works with number type.
+
+// Math.PI
+
+console.log(Math.PI); // 3.141592653589793
+
+// Basic Operations
+
+/*
+
+1. Math.abs(): Returns the absolute value of a number.
+
+2. Math.ceil(): Returns the smallest integer greater than or equal to a number.
+
+3. Math.floor(): Returns the largest integer less than or equal to a number.
+
+4. Math.round(): Returns the value of a number rounded to the nearest integer.
+
+5. Math.sqrt(): Returns the square root of a number.
+
+6. Math.cbrt(): Returns the cube root of a number.
+
+7. Math.random(): Returns a random number between 0 and 1.
+
+8. Math.trunc(): Returns the integer part of a number.
+
+9. Math.pow(): Returns the value of x to the power of y.
+
+*/
+
+console.log(Math.abs(-10)); // 10
+
+console.log(Math.ceil(10.1)); // 11
+
+console.log(Math.floor(10.9)); // 10
+
+console.log(Math.round(10.5)); // 11
+
+console.log(Math.sqrt(16)); // 4
+
+console.log(Math.cbrt(27)); // 3
+
+console.log(Math.random()); // 0.0 - 1.0
+
+console.log(Math.trunc(10.9)); // 10
+
+console.log(Math.pow(2, 3)); // 8
